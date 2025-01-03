cfa656240e040a7ad120e4707ae169b845b0afb3
AlexMorrow239 <alex.morrow239@gmail.com>
2025-01-03 13:03:54 -0500

    im warning you

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-envsubst < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+set -e
+
+# Define default value for RAILWAY_BACKEND_URL if not set
+: ${RAILWAY_BACKEND_URL:=http://localhost:3000}
+
+# Replace environment variables in nginx config
+envsubst '$RAILWAY_BACKEND_URL' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 # Start nginx
 exec "$@"
